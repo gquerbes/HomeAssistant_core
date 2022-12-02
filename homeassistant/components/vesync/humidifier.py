cc47f821a44d83f94e57ccd2dc78bef565a712c0
Gabriel <gquerbes@gmail.com>
2022-12-02 03:49:04 +0000

    wip

--- homeassistant/components/vesync/humidifier.py
+++ homeassistant/components/vesync/humidifier.py
@@ -28,11 +28,15 @@ FAN_MODE_AUTO = "auto"
 FAN_MODE_SLEEP = "sleep"
 FAN_MODE_MANUAL = "manual"
 
-MAX_HUMIDITY = 80
-MIN_HUMIDITY = 30
+MAX_HUMIDITY = 100
+MIN_HUMIDITY = 0
+
+MAX_FAN_SPEED = 9
+MIN_FAN_SPEED = 0
+
 
 PRESET_MODES = {
-    "Classic300S": [FAN_MODE_AUTO, FAN_MODE_SLEEP, FAN_MODE_MANUAL],
+    "Classic300S": [FAN_MODE_AUTO, FAN_MODE_MANUAL],
 }
 
 
@@ -76,6 +80,7 @@ class VeSyncHumidifierHA(VeSyncDevice, HumidifierEntity):
 
     _attr_device_class = HumidifierDeviceClass.HUMIDIFIER
     _attr_supported_features = HumidifierEntityFeature.MODES
+    last_known_fan_speed = 0
 
     def __init__(self, humidifier):
         """Initialize the VeSync humidity device."""
@@ -90,27 +95,35 @@ class VeSyncHumidifierHA(VeSyncDevice, HumidifierEntity):
     @property
     def target_humidity(self) -> int:
         """Return the desired humidity set point."""
-        return int(self.smarthumidifier.auto_humidity)
+        if self.smarthumidifier.auto_enabled:
+            return int(self.smarthumidifier.auto_humidity)
+        return self.last_known_fan_speed
 
     @property
     def max_humidity(self) -> int:
         """Return the MAX humidity of this fan."""
-        return MAX_HUMIDITY
+        if self.smarthumidifier.auto_enabled:
+            return MAX_HUMIDITY
+        return MAX_FAN_SPEED
 
     @property
     def min_humidity(self) -> int:
         """Return the MIN humidity of this fan."""
-        return MIN_HUMIDITY
+        if self.smarthumidifier.auto_enabled:
+            return MIN_HUMIDITY
+        return MIN_FAN_SPEED
 
     @property
     def mode(self) -> str | None:
         """Return the mist level of this fan."""
-        return self.smarthumidifier.mist_level
+        if self.smarthumidifier.auto_enabled:
+            return FAN_MODE_AUTO
+        return FAN_MODE_MANUAL
 
     @property
     def available_modes(self) -> list[str] | None:
         """Return the list of available modes."""
-        return [FAN_MODE_AUTO, FAN_MODE_MANUAL, FAN_MODE_SLEEP]
+        return [FAN_MODE_MANUAL, FAN_MODE_AUTO]
 
     @property
     def device_class(self) -> str | None:
@@ -121,7 +134,12 @@ class VeSyncHumidifierHA(VeSyncDevice, HumidifierEntity):
         """Set new target humidity."""
         if not self.smarthumidifier.is_on:
             self.smarthumidifier.turn_on()
-        self.smarthumidifier.set_humidity(int(humidity))
+
+        if self.smarthumidifier.auto_enabled:
+            self.smarthumidifier.set_humidity(int(humidity))
+        else:
+            self.smarthumidifier.set_mist_level(int(humidity))
+            self.last_known_fan_speed = int(humidity)
 
         self.schedule_update_ha_state()
 
@@ -136,11 +154,11 @@ class VeSyncHumidifierHA(VeSyncDevice, HumidifierEntity):
             self.smarthumidifier.turn_on()
 
         if mode == FAN_MODE_AUTO:
-            self.smarthumidifier.set_humidity_mode(FAN_MODE_AUTO)
+            self.smarthumidifier.set_auto_mode()
         if mode == FAN_MODE_MANUAL:
-            self.smarthumidifier.set_humidity_mode(FAN_MODE_MANUAL)
+            self.smarthumidifier.set_manual_mode()
         elif mode == FAN_MODE_SLEEP:
-            self.smarthumidifier.sleep_mode(FAN_MODE_SLEEP)
+            self.smarthumidifier.sleep_mode()
 
         self.schedule_update_ha_state()
 
